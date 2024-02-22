7e6ba1ebe6323b378498e5f08672bf2fff16ee1f
shan3274 <khanshan949@gmail.com>
2024-02-23 02:21:35 +0530

    added phone footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = ({ scrollLength, currHeight }) => {
       whileInView={{ opacity: 1 }}
       animate={{ y: 50 }}
       transition={{ duration: 1 }}
-      className="w-full h-[60vh] border-t drop-shadow-lg flex flex-col items-center justify-center z-[999] bg-[#1f1f1f] relative bottom-[50px] text-white"
+      className="w-full min-h-[60vh]  border-t drop-shadow-lg flex flex-col items-center justify-center z-[999] bg-[#1f1f1f] relative bottom-[50px] text-white"
     >
       <BackgroundBeams />
       <>
@@ -21,10 +21,10 @@ const Footer = ({ scrollLength, currHeight }) => {
           whileInView={{ opacity: 1 }}
           animate={{ y: -50 }}
           transition={{ duration: 1 }}
-          className="w-[80%] h-[90%] flex items-center justify-center relative top-[50px]"
+          className="w-[80%] min-h-[54vh] flex items-center justify-center relative top-[50px] lg:flex-row flex-col"
         >
-          <div className="w-[60%] h-full  flex items-center justify-evenly">
-            <div className="w-[45%] h-[90%] flex flex-col justify-evenly text-[13px] ">
+          <div className="lg:w-[60%] min-h-[54vh]  flex items-center justify-evenly ">
+            <div className="w-[45%] h-[50vh] flex flex-col justify-evenly text-[13px] ">
               <h1 className="text-[17px] font-[700]">OUR CAMPUS</h1>
               <Link href="#">About The College</Link>
               <Link href="#">SKCE Staff Details</Link>
@@ -50,29 +50,29 @@ const Footer = ({ scrollLength, currHeight }) => {
               <Link href="#">MBA & MCA</Link>
             </div>
           </div>
-          <div className="w-[40%] h-[80%] flex flex-col justify-evenly items-center text-[15px]">
+          <div className="lg:w-[40%] w-[100%] h-[80%] flex flex-col justify-evenly lg:gap-0 gap-2 mb-10 lg:items-center text-[15px]">
             <h1>Contact us</h1>
             <input
               type="text"
-              className="bg-transparent border-b p-5 w-[80%] h-[40px]"
+              className="bg-transparent border-b p-5 lg:w-[80%] h-[40px]"
               placeholder="Name"
             />
             <input
               type="Email"
-              className="bg-transparent border-b p-5 w-[80%] h-[40px]"
+              className="bg-transparent border-b p-5 lg:w-[80%] h-[40px]"
               placeholder="Email"
             />
             <input
               type="text"
-              className="bg-transparent border-b p-5 w-[80%] h-[40px]"
+              className="bg-transparent border-b p-5 lg:w-[80%] h-[40px]"
               placeholder="phone"
             />
             <textarea
               name=""
               placeholder="Reason"
-              className="resize-none bg-transparent border-b p-5 w-[80%] h-[100px]"
+              className="resize-none bg-transparent border-b p-5 lg:w-[80%] h-[100px]"
             ></textarea>
-            <button className="w-[80%] h-[40px] border duration-500 hover:bg-white hover:text-black">
+            <button className="lg:w-[80%] h-[40px] border duration-500 hover:bg-white hover:text-black">
               send
             </button>
           </div>
@@ -82,7 +82,7 @@ const Footer = ({ scrollLength, currHeight }) => {
           whileInView={{ opacity: 1 }}
           animate={{ y: -50 }}
           transition={{ duration: 1 }}
-          className="w-full h-[10%] flex items-center justify-center border-t relative top-[50px]"
+          className="w-full h-[10vh] lg:h-[10%] flex items-center justify-center border-t relative top-[50px]"
         >
           <p className="">© 2024 Sree Krishna College Of Engineering</p>
         </motion.div>
