f074ca1233ef6aaa6893fc941501bd670115ff10
shan3274 <khanshan949@gmail.com>
2024-02-28 22:10:33 +0530

    add all

--- components/Chairman.jsx
+++ components/Chairman.jsx
@@ -1,29 +1,60 @@
-import { BackgroundBeams } from "@/utils/background-beams";
-import Image from "next/image";
-import React, { useEffect, useState } from "react";
+"use client";
+import { motion } from "framer-motion";
+import React from "react";
+import { ImagesSlider } from "@/components/Imageslider";
 
 const Chairman = () => {
-  const [interval, setInterval] = useState(0);
-  useEffect(() => {
-    let i = 0;
-    setInterval(() => {
-      setInterval(interval + 1);
-    }, 1000);
-  }, []);
-  console.log(interval);
+  const images = [
+    "https://images.unsplash.com/photo-1485433592409-9018e83a1f0d?q=80&w=1814&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1483982258113-b72862e6cff6?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1482189349482-3defd547e0e9?q=80&w=2848&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  ];
+
   let img1 = "https://www.sreekrishnaengcollege.com/assets/img/chairman.jpg";
+  let img2 = "https://www.sreekrishnaengcollege.com/assets/img/library7.jpg";
+  let img3 = "https://www.sreekrishnaengcollege.com/assets/img/library6.jpg";
+
   return (
-    <div className="w-[full] h-[80vh] bg-[#dedede] relative overflow-hidden flex items-center justify-center">
-      <BackgroundBeams />
-      <Image
-        loader={() => (img1 = img1)}
-        src={img1}
-        width={0}
-        height={0}
-        className="w-[80%] object-cover z-[1]  drop-shadow-2xl relative "
-      />
-    </div>
+    <ImagesSlider className="h-[40rem]" images={images}>
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: -80,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 0.6,
+        }}
+        className="z-50 flex flex-col justify-center items-center"
+      >
+        <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+          The hero section slideshow <br /> nobody asked for
+        </motion.p>
+        <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+          <span>Join now →</span>
+          <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
+        </button>
+      </motion.div>
+    </ImagesSlider>
   );
 };
 
+const Images = ({ img }) => {
+  return (
+    <motion.img
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      transition={{
+        duration: 0.4,
+        type: "spring",
+      }}
+      src={img}
+      className="w-[50%] rounded-xl drop-shadow-xl"
+      alt=""
+    />
+  );
+};
 export default Chairman;
